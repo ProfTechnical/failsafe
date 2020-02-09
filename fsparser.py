550e874753069951e96624b306c5f161b984b146
Graham Greene <grahamgreene22@gmail.com>
2020-02-09 12:02:26 -0500

    Refactoring of Messages
    Raw lines added to messages for reading purposes
    index refactored to line_num

--- fsparser.py
+++ fsparser.py
@@ -20,21 +20,21 @@ def parse_from_log(parse_log):
         system.clear_messages()
 
     with open(sel_log.path) as log:
-        index = 1
+        line_num = 1
         for line in log:
             tag = re.compile(r"\[|\]\[?") #Checks for "[" or "]" with or without a "[" afterwards
             message = tag.split(line)
 
             if message[0] == '':
-                parse_subsystem_message(message, index)
+                parse_subsystem_message(line, message, line_num)
             else:
-                parse_system_message(message, index)
-            index+=1
+                parse_system_message(line, message, line_num)
+            line_num+=1
         log.close()
 
     fsanalyzer.import_subsystems(get_subsystems())
 
-def parse_subsystem_message(message, index):
+def parse_subsystem_message(line, message, line_num):
     name = message[2]
     subsystem = None
     for system in subsystems:
@@ -42,23 +42,23 @@ def parse_subsystem_message(message, index):
             subsystem = system
             break
     if subsystem == None:
-        print("Invalid subsystem: " + name + " at line " + str(index))
+        print("Invalid subsystem: " + name + " at line " + str(line_num))
         return
     else:
         level = message[1]
         if level == "Debug":
-            subsystem.add_debug(index)
+            subsystem.add_debug(line, line_num)
         elif level == "Info":
-            subsystem.add_info(index)
+            subsystem.add_info(line, line_num)
         elif level == "Warning":
-            subsystem.add_warning(index)
+            subsystem.add_warning(line, line_num)
         elif level == "Error":
-            subsystem.add_error(index)
+            subsystem.add_error(line, line_num)
         else:
-            print("Invalid message level: " + level + " at line " + str(index))
+            print("Invalid message level: " + level + " at line " + str(line_num))
             return
 
-def parse_system_message(message, index):
+def parse_system_message(line, message, line_num):
     pass
 
 def get_subsystems():
--- subsystem.py
+++ subsystem.py
@@ -6,19 +6,19 @@ class Subsystem:
     
     def __init__(self, name):
         self.name = name
-        self.messages = [[],[],[],[]]
+        self.messages = [[[]],[[]],[[]],[[]]]
     
-    def add_debug(self, line_num):
-        self.messages[self.debug_level].append(line_num)
+    def add_debug(self, msg, line_num):
+        self.messages[self.debug_level].append([msg, line_num])
 
-    def add_info(self, line_num):
-        self.messages[self.info_level].append(line_num)
+    def add_info(self, msg, line_num):
+        self.messages[self.info_level].append([msg, line_num])
     
-    def add_warning(self, line_num):
-        self.messages[self.warning_level].append(line_num)
+    def add_warning(self, msg, line_num):
+        self.messages[self.warning_level].append([msg, line_num])
     
-    def add_error(self, line_num):
-        self.messages[self.error_level].append(line_num)
+    def add_error(self, msg, line_num):
+        self.messages[self.error_level].append([msg, line_num])
 
     def get_messages(self):
         return self.messages
